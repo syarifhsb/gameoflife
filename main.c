6e5c9abd4ee5d7bfb7ada120e2380fc67e9e8d83
syarif-hsb <hidayatullah.hsb@gmail.com>
2024-04-16 08:29:24 +0200

    Possibility to initialize randomly or through init file. Change update period through macro TIME_PERIOD

--- main.c
+++ main.c
@@ -4,9 +4,12 @@
 #include <assert.h>
 #include <time.h>
 
+/* #define USE_INIT */
+#define TIME_UPDATE 50     /* in ms */
 #define CELL_SIZE 6
+#define CELLS_WIDTH  50
+#define CELLS_HEIGHT 50
 
-/* int check_neighbor(bool *cells, int i, int j) */ 
 bool *cells, *next_cells;
 void update_cell(int cell_n, int cell_m)
 {
@@ -42,14 +45,12 @@ void update_cell(int cell_n, int cell_m)
 
 int main(int argc, char *argv[])
 {
-  SDL_Init( SDL_INIT_EVENTS | SDL_INIT_VIDEO );
-  SDL_Window *window;
-  SDL_Renderer *renderer;
-
-  FILE *init_file = fopen("./init.txt", "r");
-  assert(init_file != NULL);
   int init_n = 0;
   int init_m = 0;
+
+#ifdef USE_INIT
+  FILE *init_file = fopen("./init.txt", "r");
+  assert(init_file != NULL);
   char c;
 
   while ((c = fgetc(init_file)) != EOF) {
@@ -60,27 +61,36 @@ int main(int argc, char *argv[])
       init_m++;
     }
   }
+  cells = calloc( init_m * init_n, sizeof( *cells ) );
+  next_cells = calloc( init_m * init_n, sizeof( *next_cells ) );
+
   int ret = fseek(init_file, 0, SEEK_SET);
   assert(ret == 0);
 
-  cells = calloc( init_m * init_n, sizeof( *cells ) );
-  next_cells = calloc( init_m * init_n, sizeof( *next_cells ) );
   int counter = 0;
 
-/* #if 0 */
-/*   while ((c = fgetc(init_file)) != EOF) { */
-/*     if (c == '\n') continue; */
-/*     else if (c == '*') cells[counter] = true; */
+  while ((c = fgetc(init_file)) != EOF) {
+    if (c == '\n') continue;
+    else if (c == '*') cells[counter] = true;
 
-/*     counter++; */
-/*   } */
-/* #else */
+    counter++;
+  }
+#else
+  init_n = CELLS_HEIGHT;
+  init_m = CELLS_WIDTH;
+  cells = calloc( init_m * init_n, sizeof( *cells ) );
+  next_cells = calloc( init_m * init_n, sizeof( *next_cells ) );
+
   srand(time(NULL));
   for (int i = 0; i < init_n*init_m; ++i) {
     if (rand() < 0.3 * RAND_MAX)
       cells[i] = true;
   }
-/* #endif */
+#endif
+
+  SDL_Init( SDL_INIT_EVENTS | SDL_INIT_VIDEO );
+  SDL_Window *window;
+  SDL_Renderer *renderer;
 
   window = SDL_CreateWindow( "Conway's game of life", 
       SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -92,8 +102,7 @@ int main(int argc, char *argv[])
 
   SDL_Event e;
   int loop = 1;
-  struct timespec t1, t2;
-  clock_gettime(CLOCK_REALTIME, &t1);
+  struct timespec t1 = {0}, t2;
   while ( loop ) {
     if ( SDL_PollEvent ( &e ) ) {
       switch ( e.type ) {
@@ -104,27 +113,27 @@ int main(int argc, char *argv[])
       }
     }
 
-    SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
-    SDL_RenderClear( renderer );
-    SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
-
-    SDL_Rect cell;
-    for ( int i = 0; i < init_n; i++) {
-      for ( int j = 0; j < init_m; j++) {
-        if ( cells[i * init_n + j] == true ) {
-          cell.x = CELL_SIZE * j;
-          cell.y = CELL_SIZE * i;
-          cell.w = cell.h = CELL_SIZE;
-          SDL_RenderFillRect( renderer, &cell );
-        }
-      }
-    }
-    SDL_RenderPresent( renderer );
     clock_gettime(CLOCK_REALTIME, &t2);
     if (((t2.tv_sec - t1.tv_sec) * 1000 + 
-          (t2.tv_nsec - t1.tv_nsec) / 1000000) >= 100) {
-      update_cell(init_n, init_m);
+          (t2.tv_nsec - t1.tv_nsec) / 1000000) >= TIME_UPDATE) {
       t1 = t2;
+      SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
+      SDL_RenderClear( renderer );
+      SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
+
+      SDL_Rect cell;
+      for ( int i = 0; i < init_n; i++) {
+        for ( int j = 0; j < init_m; j++) {
+          if ( cells[i * init_m + j] == true ) {
+            cell.x = CELL_SIZE * j;
+            cell.y = CELL_SIZE * i;
+            cell.w = cell.h = CELL_SIZE;
+            SDL_RenderFillRect( renderer, &cell );
+          }
+        }
+      }
+      SDL_RenderPresent( renderer );
+      update_cell(init_n, init_m);
     }
   }
 
